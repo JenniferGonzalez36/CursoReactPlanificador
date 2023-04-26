76954fa6c71c8c741cb9f8a940ea05801fe4b0e5
Jennifer González <jenniferjacqueline936@gmail.com>
2023-04-26 14:45:22 -0600

    Adding the circular progress bar

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import "react-circular-progressbar/dist/styles.css";
 
 const ControlPresupuesto = ({ budget, spends }) => {
 
     const [available, setAvailable] = useState(0);
     const [spent, setSpent] = useState(0);
+    const [percentage, setPercentage] = useState(0);
 
     useEffect(() => {
         const totalSpent = spends.reduce((total, spend) => spend.quantity + total, 0);
-        setSpent(totalSpent);
-        setAvailable(budget-totalSpent);
+        const totalAvailable = budget - totalSpent;
+
+        const newPercentage = ((budget - totalAvailable) / budget * 100).toFixed(2);
+
+        setAvailable(totalAvailable);
+        setPercentage(newPercentage);
+        setTimeout(() => {
+            setSpent(totalSpent);
+        }, 1500);
     }, [spends]);
 
     const formatQuantity = (quantity) => {
@@ -22,7 +32,15 @@ const ControlPresupuesto = ({ budget, spends }) => {
     return (
         <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
             <div>
-                <p>Grafica aquí</p>
+                <CircularProgressbar
+                    styles={buildStyles({
+                        pathColor: "#3B82F6",
+                        trailColor: "#F5F5F5",
+                        textColor: "#3B82F6"
+                    })}
+                    value={percentage}
+                    text={`${percentage}% Gastado`}
+                />
             </div>
             <div className='contenido-presupuesto'>
                 <p>
